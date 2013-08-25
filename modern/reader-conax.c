3aff00e91e1c9db9f7dac9d1658e7307cf780da2
gorgone.impertinence <gorgone.impertinence@1c17c7de-f7fc-4fa4-bb49-34168c3c6d3e>
2013-08-25 12:46:03 +0000

    sync to #8866

--- modern/reader-conax.c
+++ modern/reader-conax.c
@@ -12,11 +12,11 @@ static int32_t RSA_CNX(struct s_reader *reader, unsigned char *msg, unsigned cha
   unsigned char data[64];
 
     /*prefix size*/
-    pre_size = 2 + 4 + msg[5]; 
+    pre_size = 2 + 4 + msg[5];
     /*size of data to decryption*/
     if(msg[1] > (pre_size-2))
-      size = msg[1] - pre_size + 2; 
-    
+      size = msg[1] - pre_size + 2;
+
     if(cta_lr > (pre_size + size) &&
        size >= modbytes && size < 128)
     {
@@ -28,7 +28,7 @@ static int32_t RSA_CNX(struct s_reader *reader, unsigned char *msg, unsigned cha
         if (ctx == NULL) {
           rdr_debug_mask(reader, D_READER, "RSA Error in RSA_CNX");
         }
-        
+
         /*RSA first round*/
         BN_bin2bn (mod, modbytes, bn_mod); // rsa modulus
         BN_bin2bn (exp, expbytes, bn_exp); // exponent
@@ -36,21 +36,21 @@ static int32_t RSA_CNX(struct s_reader *reader, unsigned char *msg, unsigned cha
         BN_mod_exp (bn_res, bn_data, bn_exp, bn_mod, ctx);
 
         n = BN_bn2bin (bn_res, data);
-        
+
         size -= modbytes; //3
         pre_size += modbytes;
         /*Check if second round is needed*/
         if(0 < size)
         {
           /*check if length of data from first RSA round will be enough to padding rest of data*/
-          if((n+size) >= modbytes) 
+          if((n+size) >= modbytes)
           {
             /*RSA second round*/
             /*move the remaining data at the beginning of the buffer*/
-            memcpy(msg, msg+pre_size, size); 
+            memcpy(msg, msg+pre_size, size);
             /*padding buffer with data from first round*/
-            memcpy(msg+size, data+(n - (modbytes-size)), modbytes-size); 
-            
+            memcpy(msg+size, data+(n - (modbytes-size)), modbytes-size);
+
             BN_bin2bn (msg, modbytes, bn_data);
             BN_mod_exp (bn_res, bn_data, bn_exp, bn_mod, ctx);
             n = BN_bn2bin (bn_res, data);
@@ -67,7 +67,7 @@ static int32_t RSA_CNX(struct s_reader *reader, unsigned char *msg, unsigned cha
     }
     else
       ret = -2; /*wrong size of data*/
-     
+
     return ret;
 }
 
@@ -161,6 +161,8 @@ static int32_t conax_card_init(struct s_reader * reader, ATR *newatr)
 
   if ((n=read_record(reader, ins82, ins82+5, cta_res))<=0) return ERROR; // read serial
 
+  reader->nprov = 0;
+
   for (j=0, i=2; i<n; i+=cta_res[i+1]+2)
     switch(cta_res[i])
     {
@@ -171,12 +173,11 @@ static int32_t conax_card_init(struct s_reader * reader, ATR *newatr)
         else {
           memcpy(reader->sa[j], &cta_res[i+5], 4);
           j++;
+          reader->nprov++;
         }
         break;
     }
 
-  // we have one provider, 0x0000
-  reader->nprov = 1;
   memset(reader->prid, 0x00, sizeof(reader->prid));
 
   rdr_log_sensitive(reader, "type: Conax, caid: %04X, serial: {%llu}, hex serial: {%02x%02x%02x%02x}, card: v%d",
@@ -188,7 +189,7 @@ static int32_t conax_card_init(struct s_reader * reader, ATR *newatr)
   for (j=0; j<reader->nprov; j++)
   {
     rdr_log(reader, "Provider: %d  Provider-Id: %06X", j+1, b2i(4, reader->prid[j]));
-    rdr_log(reader, "Provider: %d  SharedAddress: %08X", j+1, b2i(4, reader->sa[j]));
+    rdr_log_sensitive(reader, "Provider: %d  SharedAddress: {%08X}", j+1, b2i(4, reader->sa[j]));
   }
 
   return OK;
@@ -247,7 +248,7 @@ static int32_t conax_do_ecm(struct s_reader * reader, const ECM_REQUEST *er, str
         else
           rc = -4; /*card has no right to decode this channel*/
       }
-      
+
       if(0 == rc)
       for(i = 0;  i < cta_lr-2 && num_dw < 2; i+=cta_res[i+1]+2)
       {
@@ -295,7 +296,7 @@ static int32_t conax_do_ecm(struct s_reader * reader, const ECM_REQUEST *er, str
       }
     }
   }
-  
+
   switch(rc)
   {
     case -1:
@@ -310,7 +311,7 @@ static int32_t conax_do_ecm(struct s_reader * reader, const ECM_REQUEST *er, str
       rdr_log(reader, "card has no right to decode this channel");
     break;
   }
-  
+
   /* answer 9011 - conax smart card need reset */
   if(2<=cta_lr && 0x90==cta_res[cta_lr-2] &&
      0x11==cta_res[cta_lr-1])
@@ -361,44 +362,50 @@ static int32_t conax_get_emm_type(EMM_PACKET *ep, struct s_reader * rdr)
 	}
 }
 
-static void conax_get_emm_filter(struct s_reader * rdr, uchar *filter)
+static struct s_csystem_emm_filter* conax_get_emm_filter(struct s_reader *rdr)
 {
-	int32_t idx = 2;
+  struct s_csystem_emm_filter *filters = rdr->csystem.emm_filters;
 
-	filter[0]=0xFF;	//header
-	filter[1]=0;		//filter count
+  if (filters == NULL) {
+    const unsigned int max_filter_count = 3;
+    if (!cs_malloc(&rdr->csystem.emm_filters, max_filter_count * sizeof(struct s_csystem_emm_filter)))
+      return NULL;
 
-	filter[idx++]=EMM_GLOBAL;
-	filter[idx++]=1; // FIXME: dont see any conax global EMM yet
-	filter[idx+0]    = 0x82;
-	filter[idx+0+16] = 0xFF;
-	filter[idx+8]    = 0x70;
-	filter[idx+8+16] = 0xFF;
-	filter[1]++;
-	idx += 32;
+    filters = rdr->csystem.emm_filters;
+    rdr->csystem.emm_filter_count = 0;
+    memset(filters, 0x00, max_filter_count * sizeof(struct s_csystem_emm_filter));
 
-	filter[idx++]=EMM_SHARED;
-	filter[idx++]=0;
-	filter[idx+0]    = 0x82;
-	filter[idx+0+16] = 0xFF;
-	filter[idx+8]    = 0x70;
-	filter[idx+8+16] = 0xFF;
-	memcpy(filter+idx+4, rdr->sa[0], 4);
-	memset(filter+idx+4+16, 0xFF, 4);
-	filter[1]++;
-	idx += 32;
+    int32_t idx = 0;
 
-	filter[idx++]=EMM_UNIQUE;
-	filter[idx++]=0;
-	filter[idx+0]    = 0x82;
-	filter[idx+0+16] = 0xFF;
-	filter[idx+8]    = 0x70;
-	filter[idx+8+16] = 0xFF;
-	memcpy(filter+idx+4, rdr->hexserial + 2, 4);
-	memset(filter+idx+4+16, 0xFF, 4);
-	filter[1]++;
+    filters[idx].type = EMM_GLOBAL;
+    filters[idx].enabled   = 0; // FIXME: dont see any conax global EMM yet
+    filters[idx].filter[0] = 0x82;
+    filters[idx].mask[0]   = 0xFF;
+    filters[idx].filter[8] = 0x70;
+    filters[idx].mask[8]   = 0xFF;
+    idx++;
 
-	return;
+    filters[idx].type = EMM_SHARED;
+    filters[idx].enabled  = 1;
+    filters[idx].filter[0] = 0x82;
+    filters[idx].mask[0]   = 0xFF;
+    memcpy(&filters[idx].filter[4], rdr->sa[0], 4);
+    memset(&filters[idx].mask[4], 0xFF, 4);
+
+    idx++;
+
+    filters[idx].type = EMM_UNIQUE;
+    filters[idx].enabled  = 1;
+    filters[idx].filter[0] = 0x82;
+    filters[idx].mask[0]   = 0xFF;
+    memcpy(&filters[idx].filter[4], rdr->hexserial + 2, 4);
+    memset(&filters[idx].mask[4], 0xFF, 4);
+    idx++;
+
+    rdr->csystem.emm_filter_count = idx;
+  }
+
+  return filters;
 }
 
 static int32_t conax_do_emm(struct s_reader * reader, EMM_PACKET *ep)
