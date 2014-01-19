4bfebc58e7e5b726ec8d8d5cf8ad3081e5922bbc
gorgone.impertinence <gorgone.impertinence@1c17c7de-f7fc-4fa4-bb49-34168c3c6d3e>
2014-01-19 03:26:11 +0000

    sync to 9401

--- modern/module-webif.c
+++ modern/module-webif.c
@@ -3170,7 +3170,7 @@ static char *send_oscam_user_config(struct templatevars *vars, struct uriparams 
 	tpl_printf(vars, TPLADD, "TOTAL_ONLINE", "%d", online_users);
 
 
-	float ecmsum = first_client->cwfound + first_client->cwnot + first_client->cwignored + first_client->cwtout + first_client->cwcache; //dont count TUN its included
+	float ecmsum = first_client->cwfound + first_client->cwnot + first_client->cwtout + first_client->cwcache; //dont count TUN its included
 	if(ecmsum < 1) 	ecmsum = 1;
 	float ecmpos = first_client->cwfound + first_client->cwcache; // dont count TUN its included
 	if(ecmpos < 1) ecmpos = 1;
@@ -3221,7 +3221,7 @@ static char *send_oscam_user_config(struct templatevars *vars, struct uriparams 
 	tpl_printf(vars, TPLADD, "TOTAL_CWCACHE", "%d", first_client->cwcache);
 	tpl_printf(vars, TPLADD, "TOTAL_CWTUN", "%d", first_client->cwtun);
 	tpl_printf(vars, TPLADD, "TOTAL_CWPOS", "%d", first_client->cwfound + first_client->cwcache);
-	tpl_printf(vars, TPLADD, "TOTAL_CWNEG", "%d", first_client->cwnot + first_client->cwignored + first_client->cwtout);
+	tpl_printf(vars, TPLADD, "TOTAL_CWNEG", "%d", first_client->cwnot + first_client->cwtout);
 
 
 	tpl_printf(vars, TPLADD, "REL_CWOK", "%.2f", first_client->cwfound * 100 / ecmsum);
@@ -3231,7 +3231,7 @@ static char *send_oscam_user_config(struct templatevars *vars, struct uriparams 
 	tpl_printf(vars, TPLADD, "REL_CWCACHE", "%.2f", first_client->cwcache * 100 / ecmsum);
 	tpl_printf(vars, TPLADD, "REL_CWTUN", "%.2f", first_client->cwtun * 100 / ecmsum);
 	tpl_printf(vars, TPLADD, "REL_CWPOS", "%.2f", (first_client->cwfound + first_client->cwcache) * 100 / ecmsum);
-	tpl_printf(vars, TPLADD, "REL_CWNEG", "%.2f", (first_client->cwnot + first_client->cwignored + first_client->cwtout) * 100 / ecmsum);
+	tpl_printf(vars, TPLADD, "REL_CWNEG", "%.2f", (first_client->cwnot + first_client->cwtout) * 100 / ecmsum);
 	tpl_printf(vars, TPLADD, "REL_CWPOSOK", "%.2f", first_client->cwfound * 100 / ecmpos);
 	tpl_printf(vars, TPLADD, "REL_CWPOSCACHE", "%.2f", first_client->cwcache * 100 / ecmpos);
 	tpl_printf(vars, TPLADD, "REL_CWNEGNOK", "%.2f", first_client->cwnot * 100 / ecmneg);
@@ -4316,7 +4316,6 @@ static char *send_oscam_status(struct templatevars * vars, struct uriparams * pa
 					if(cl->typ == 's')
 					{
 						usr = username(cl);
-						tpl_printf(vars, TPLADD, "RUNAS", "Run as: %s", xml_encode(vars, usr));
 					}
 				}
 			}
@@ -6619,6 +6618,7 @@ static int32_t process_request(FILE * f, IN_ADDR_T in)
 			tpl_printf(vars, TPLADD, "STARTDATE", "%02d.%02d.%02d", st.tm_mday, st.tm_mon + 1, st.tm_year % 100);
 			tpl_printf(vars, TPLADD, "STARTTIME", "%02d:%02d:%02d", st.tm_hour, st.tm_min, st.tm_sec);
 			tpl_printf(vars, TPLADD, "PROCESSID", "%d", getpid());
+			tpl_addVar(vars, TPLADD, "RUNAS", urlencode(vars, username(first_client)));
 
 			time_t now = time((time_t *)0);
 			// XMLAPI
